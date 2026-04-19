169f2a1771eeef6d2361f172162f054aad3b6f42
naruse-sangi <274093881+naruse-sangi@users.noreply.github.com>
2026-04-19 18:22:06 +0900

    ﻿引き算の結果表示を実装した

--- kadai.ps1
+++ kadai.ps1
@@ -3,4 +3,4 @@ Param([int]$value1, [int]$value2)
 $ans = $value1 + $value2
 Write-Output "$value1 + $value2 = $ans"
 $ans = $value1 - $value2
-Write-Output "$value1 + $value2 = $ans"
+Write-Output "$value1 - $value2 = $ans"
